cbb0c1083caf0833e95281e98b5c0a05f8c040e8
Hector Brown <hectorjbrown@protonmail.com>
2022-12-04 17:02:42 +0000

    day 4

---
+++ 4/p2.py
@@ -0,0 +1,11 @@
+PATH = "4/data.txt"
+
+pairs = [
+    [
+        sum([2**i for i in range(*[int(z) + i for i, z in enumerate(y.split("-"))])])
+        for y in x[:-1].split(",")
+    ]
+    for x in open(PATH, "r").readlines()
+]
+
+print(sum([pair[0] | pair[1] != pair[0] ^ pair[1] for pair in pairs]))
